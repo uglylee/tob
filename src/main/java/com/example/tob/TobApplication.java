e051a8108ed115c279454cc49386692395c2474a
赵李 <zhaoli@picb.ac.cn>
2020-12-28 22:25:25 +0800

    first commit

---
+++ src/main/java/com/example/tob/TobApplication.java
@@ -0,0 +1,13 @@
+package com.example.tob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tob/TobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
